014ed56fcb5f6f5655e5b07ce50bbb9fb7fa920e
Carlos Sanabria <carlossanabria636@gmail.com>
2024-04-19 14:45:22 -0400

    Add an option to do an Amazon search

--- README.md
+++ README.md
@@ -1,23 +1,31 @@
-# GoogleYoutube-search-bash-script-for-Windows
+# GoogleYoutubeAmazon-search-bash-script-for-Windows
 
 A bash script for searching on google or youtube videos you can do by yourself without installing anything else than bash
+
+## The idea behind the code
+
+I just needed a fast way to search for certain things on websites that I usually use when I turn on the PC
 
 ## HOW IT WORKS
 
 The code works by typing an alias you will make later and adding 2 arguments:
 
-- The first argument is `g` or `yt` for a google search or youtube video respectively
+- The first argument is `g` for a google search, `yt` for a youtube video, or `a` for an amazon search
 - The second argument is going to be whatever you are searching for e.g., `how to make a cake`
 
 #### Some examples of the full command are
 
-- For a google search:
+- For a Google search:
   ```
   $ goose g how to make a cake
   ```
 - For a Youtube search:
   ```
   $ goose yt best ways to make money real quick tutorial
+  ```
+- For an Amazon search:
+  ```
+  $ goose a pc speakers
   ```
   As you can see the alias that i used was `goose`which stands for`google search engine` but you can use whatever you want.
 
--- search_engine.sh
+++ search_engine.sh
@@ -2,14 +2,15 @@
 
 # echo "MY first bash script!"
 
-#greeting just to play a bit
-USERNAME=$(whoami) #prints pc user
+# greeting just to play a bit
+# prints pc user
+USERNAME=$(whoami) 
 GREET="Good Morning, $USERNAME 😹👍"
 
-#echo prints the greeting
+# echo prints the greeting
 echo $GREET
 
-#sample search: "goose g how to make a cake"
+# sample search: "goose g how to make a cake"
 search_google() {
     # this line checks for the parameter that is sent from the IF statement
     # this parameter is just "$1" since all the parameters are sent as one from the IF statement("$*")
@@ -25,20 +26,31 @@ search_google() {
 # sample search: "goose yt [youtube-search]"
 search_youtube() {
     query="$1"
-    echo "Searching on youtube for: $query"
+    echo "Searching on Youtube for: $query"
     query=$(echo "$query" | sed 's/ /+/g')
     start "https://www.youtube.com/results?search_query=$query"
+}
+
+# sample search: "goose a [amazon-search]"
+search_amazon() {
+    query="$1"
+    echo "Searching on Amazon for: $query"
+    query=$(echo "$query" | sed 's/ /+/g')
+    start "https://www.amazon.com/s?k=$query"
 }
 
 
 if [ "$1" = "g" ]; then 
     # shift sets the next positional parameter ("$2") to the left positional parameter by one position("$1")
     shift
-    #calls the function and passes all the following parameters for the function to receive it as one
+    # calls the function and passes all the following parameters for the function to receive it as one
     search_google "$*"
 elif [ "$1" = "yt" ]; then
     shift
     search_youtube "$*"
+elif [ "$1" = "a" ]; then
+    shift
+    search_amazon "$*"
 else
     echo "Invalid command"
 fi
